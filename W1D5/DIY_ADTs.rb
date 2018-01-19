a6e9979a16e3b43d1f626ae0568339225f555772
Yang Zhou <yzhou3991@gmail.com>
2018-01-19 04:14:36 -0500

    create class for Map

--- W1D5/DIY_ADTs.rb
+++ W1D5/DIY_ADTs.rb
@@ -33,3 +33,34 @@ class Queue
     @queue.dup
   end
 end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    pair_index = @map.index {|pair| pair[0] == key}
+    pair_index ? @map[pair_index][1] = value : @map.push([key, value])
+    [key, value]
+  end
+
+  def lookup(key)
+    @map.each {|pair| return pair[1] if pair[0] == key}
+    nil
+  end
+
+  def remove(key)
+    @map.reject! {|pair| pair[0] == key}
+    nil
+  end
+
+  def show
+    deep_dup(@map)
+  end
+
+  private
+  def deep_dup(arr)
+    arr.map { |el| el.is_a?(Array) ? deep_dup(el) : el }
+  end
+end
