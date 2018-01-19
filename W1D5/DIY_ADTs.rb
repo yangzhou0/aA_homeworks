ca7dcf8b1f8f6f3a15b123e1ad1549353c2ca162
Yang Zhou <yzhou3991@gmail.com>
2018-01-19 04:09:09 -0500

    create Class for stack

--- W1D5/DIY_ADTs.rb
+++ W1D5/DIY_ADTs.rb
@@ -1,17 +1,17 @@
 class Stack
   def initialize
-    # create ivar to store stack here!
+    @stack = Array.new
   end
 
   def add(el)
-    # adds an element to the stack
+    @stack << el
   end
 
   def remove
-    # removes one element from the stack
+    @stack.pop
   end
 
   def show
-    # return a copy of the stack
+    @stack.dup
   end
 end
