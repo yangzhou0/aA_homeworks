cafff03037b2d8399c9ce9d31cfd9c6e343a1e7c
Yang Zhou <yzhou3991@gmail.com>
2018-01-19 00:53:12 -0500

    created homework filefor W1D5

---
+++ W1D5/DIY_ADTs.rb
@@ -0,0 +1,17 @@
+class Stack
+  def initialize
+    # create ivar to store stack here!
+  end
+
+  def add(el)
+    # adds an element to the stack
+  end
+
+  def remove
+    # removes one element from the stack
+  end
+
+  def show
+    # return a copy of the stack
+  end
+end
